903f0ddf50a8c7a8f4be101b382a2dfa6e41d089
ayoubben18 <ayoubbensalah2004@gmail.com>
2026-01-01 14:38:01 +0100

    Seed customer

--- src/main/java/com/crud/demo/config/DataInitializer.java
+++ src/main/java/com/crud/demo/config/DataInitializer.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
@@ -22,9 +24,15 @@ public class DataInitializer implements CommandLineRunner {
             admin.setPassword(passwordEncoder.encode("password123"));
             admin.setRole("ROLE_ADMIN");
 
-            userRepository.save(admin);
+            User customer = new User();
+            customer.setUsername("customer");
+            customer.setPassword(passwordEncoder.encode("password123"));
+            customer.setRole("ROLE_CUSTOMER");
+
+            userRepository.saveAll(List.of(admin, customer));
 
             IO.println("Test user created: admin/password123");
+            IO.println("Test customer created: customer/password123");
         }
     }
 }
