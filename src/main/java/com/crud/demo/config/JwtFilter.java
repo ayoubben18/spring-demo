8372b397015e7d6d141f5b83b949853bf1cde68a
ayoubben18 <ayoubbensalah2004@gmail.com>
2026-01-01 14:09:58 +0100

    Bind the jwt filter to the endpoints

--- src/main/java/com/crud/demo/config/JwtFilter.java
+++ src/main/java/com/crud/demo/config/JwtFilter.java
@@ -5,14 +5,20 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
 
+// this tells spring please create an instance of this filter and keep it in your container
+@Component
+@RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
-    private final JwtService jwtService = new JwtService();
+    private final JwtService jwtService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse respose, FilterChain filterChain)
@@ -25,7 +31,7 @@ public class JwtFilter extends OncePerRequestFilter {
             if (jwtService.isTokenValid(token)){
                 String username = jwtService.extractUsername(token);
 
-                var authToken = new UsernamePasswordAuthenticationToken(username, null, null);
+                var authToken = new UsernamePasswordAuthenticationToken(username, null,  Collections.emptyList());
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
--- src/main/java/com/crud/demo/config/SecurityConfig.java
+++ src/main/java/com/crud/demo/config/SecurityConfig.java
@@ -1,16 +1,20 @@
 package com.crud.demo.config;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
@@ -24,14 +28,17 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                // since we're using jwts no need for managed sessions in memory
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/products/**").permitAll()
                         .requestMatchers("/api/orders/**").authenticated()
                         .anyRequest().permitAll()
-                );
+                ).addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
